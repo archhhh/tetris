9780d0c929b287a7d49e5ce6171de2791451fe21
Artyom Chen <artychen13@gmail.com>
2019-04-21 00:35:10 +0900

    react initialization 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById("tetris");
-const context = canvas.getContext("2d");
-
-context.fillStyle = "#000000";
-context.fillRect(0, 0, canvas.width, canvas.height);
+ReactDOM.render(
+    <p>Hello World</p>,
+    document.getElementById('root')
+);
+  
