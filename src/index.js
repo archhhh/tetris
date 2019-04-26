d1d80dbfb7e45cffef81ca7e112a2d2c27b05529
Artyom Chen <artychen13@gmail.com>
2019-04-27 01:50:04 +0900

    with each frame block drops one pixel lower

--- src/index.js
+++ src/index.js
@@ -3,19 +3,56 @@ import ReactDOM from 'react-dom';
 import './style.css';
 
 class Game extends React.Component{
-
     constructor(props){
         super(props);
         this.width = this.props.width;
         this.height = this.props.height;
+        this.state = {
+            player: {
+                x: 4,
+                y: 1,
+            },
+            matrix: [
+                [0,0,0],
+                [1,1,1],
+                [0,1,0],
+            ],
+        };
+        this.last = 0;
     }
-    componentDidMount() {
+    componentDidMount = () => {
         this.canvas = this.refs.canvas;
-        this.context = this.canvas.getContext('2d');
-        this.context.fillStyle = '#000000';
-        this.context.fillRect(0,0, this.width, this.height);
+        this.context= this.canvas.getContext('2d');
+        this.context.scale(20,20);
+        this.update();
     }
-
+    draw = () => {
+        this.context= this.canvas.getContext('2d');
+        this.context.fillStyle = '#000';
+        this.context.fillRect(0,0, this.width, this.height);
+        this.drawMatrix(this.state.matrix);
+    }
+    drawMatrix = (matrix) => {
+        matrix.forEach( (row, y) => {
+            row.forEach( (value, x) => {
+                if(value !== 0)
+                {
+                    this.context.fillStyle = 'red';
+                    this.context.fillRect(x+this.state.player.x, y+this.state.player.y, 1, 1);
+                }
+            });
+        });
+    }
+    update = (time = 0) => {
+        if(time - this.last >= 1000){
+            this.last = time;
+            let x = this.state.player.x;
+            let y = this.state.player.y;
+            this.setState({player: {x: x, y: ++y}});
+        }
+        this.draw();
+        window.requestAnimationFrame(this.update);
+    } 
     render(){
         return (
             <canvas ref="canvas" width={this.width} height={this.height}></canvas>
