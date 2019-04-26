6ea442a8f79d1e2458e77eacc1c971b3d084efe9
Artyom Chen <artychen13@gmail.com>
2019-04-27 00:58:42 +0900

    canvas init

--- src/index.js
+++ src/index.js
@@ -4,16 +4,28 @@ import './style.css';
 
 class Game extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.width = this.props.width;
+        this.height = this.props.height;
+    }
+    componentDidMount() {
+        this.canvas = this.refs.canvas;
+        this.context = this.canvas.getContext('2d');
+        this.context.fillStyle = '#000000';
+        this.context.fillRect(0,0, this.width, this.height);
+    }
+
     render(){
         return (
-
+            <canvas ref="canvas" width={this.width} height={this.height}></canvas>
         );
     }
 
 }
 
 ReactDOM.render(
-    <Game />,
+    <Game width="240" height="400"/>,
     document.getElementById('root')
   );
   
